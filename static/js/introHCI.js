2f12029802d742a6a611bde53c15b6dad60e801c
Lindy Hernanfez <lindyhernandezz@gmail.com>
2018-01-25 10:25:33 -0800

    Edited js of webpage

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,34 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) {
+	//prevent the page from reloading
+	e.preventDefault();
+	// In an event handler, $(this) refers to 
+	// the object that trigerred the event
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		$(description).fadeOut();
+	}
+	// containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...")
+		$("#testjs").addClass("active");
+		$("#testjs").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
